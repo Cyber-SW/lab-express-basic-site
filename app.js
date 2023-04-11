4b267a51722acc6a179183ed9b066243eb3435b6
Shawn Wolter <woltersh@outlook.de>
2023-04-11 23:38:39 +0200

    finished lab

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 })
 
+app.get("/home", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
 app.get("/about", (req, res) => {
     res.sendFile(__dirname + "/views/about.html")
 })
